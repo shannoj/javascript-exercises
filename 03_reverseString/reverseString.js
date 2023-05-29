89591698cd1e7e2d69b44a53edd739edc3c964f0
shannoj <69207315+shannoj@users.noreply.github.com>
2023-05-29 18:24:28 +0200

    wrote a reverse string function by using a for loop to get each charater in the string and then adding it in reverse order to an array and then returning the array as a stirng

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const reverseArray = [];
+    for (var i = 0; i < string.length; i++) {
+        reverseArray.unshift(string.charAt(i));
+    }
 
+    let reversedString = reverseArray.join("");
+    return reversedString;
 };
 
 // Do not edit below this line
