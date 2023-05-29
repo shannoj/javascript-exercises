299fcfd2d8a3fe93bebd94c5e79c4e87ae195e09
shannoj <69207315+shannoj@users.noreply.github.com>
2023-05-29 16:43:45 +0200

    wrote function for repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    let x = new Array();
+    let y = 0;
+    let repeat = ''
+    while (y < num) {
+        repeat = repeat + string;
+        y += 1;
+    }
+    return repeat;
 };
 
 // Do not edit below this line
