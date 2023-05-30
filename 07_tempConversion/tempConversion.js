2675bab4cac4fc5aafc8bb7ef2d97407eb5203b1
shannoj <69207315+shannoj@users.noreply.github.com>
2023-05-30 17:37:29 +0200

    created functions that convert between farenheit and celcius, while also roounding to one digit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(int_1) {
+  celcius = ((int_1 - 32) * 5/9);
+  return (Math.round(celcius * 10) / 10)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(int_1) {
+  farenheit = (int_1 * (9/5) + 32);
+  return (Math.round(farenheit * 10) / 10)
 };
 
 // Do not edit below this line
