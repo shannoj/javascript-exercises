8021d171b30e234644c5a5f644f9d3692e5701fc
shannoj <69207315+shannoj@users.noreply.github.com>
2023-05-30 16:37:46 +0200

    wrote the function to remove values from an array using a nested for loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
-
+    let array = arguments[0];
+    let newArray = [];
+    for (var i = 0; i < array.length; i++){
+        for (var j = 1; j < arguments.length; j++){
+            if (array[i] === arguments[j]){
+                array.splice(i, 1);
+                i = -1;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
