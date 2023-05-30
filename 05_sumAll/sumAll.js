e6f7512949d911aed89b20fc83872ca2023a024c
shannoj <69207315+shannoj@users.noreply.github.com>
2023-05-30 16:55:19 +0200

    wrote function to sum all numbers inbetween two given numbers by taking the difference between the two numbers and looping over difference amount of times

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(int_1, int_2) {
 
+    if (int_1 < 0 || int_2 < 0){
+        return "ERROR";
+    }
+
+    if (typeof int_1 != typeof 1 || typeof int_2 != typeof 1){
+        return "ERROR";
+    }
+    
+    if (int_1 > int_2){
+        dif = int_1 - int_2;
+        count = 1;
+        sum = int_2 + int_1;
+        while (count < dif){
+            sum += (int_2 + count);
+            count += 1;
+        }
+        return sum;
+    }
+
+    else {
+        dif = int_2 - int_1;
+        count = 1;
+        sum = int_2 + int_1;
+        while (count < dif){
+            sum += (int_1 + count);
+            count += 1;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
